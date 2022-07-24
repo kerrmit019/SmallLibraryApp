9b1235303688db1866aab4bc1b20728f418b6ab0
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-07-24 12:35:39 +1200

    Bugfix: Remove code for closing modal which did not work

--- script.js
+++ script.js
@@ -40,13 +40,6 @@ closeModalButton.addEventListener("click", function () {
   newBookModal.style.display = "none";
 });
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
 // Set up Library  array and Book constructor
 
 let myLibrary = [];
