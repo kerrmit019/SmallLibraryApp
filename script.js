84b316fe61f3f040669b66690060f642d7596451
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-07-31 17:46:31 +1200

    BugFix: Click outside modal to close fixed

--- script.js
+++ script.js
@@ -41,6 +41,13 @@ closeModalButton.addEventListener("click", function () {
   newBookModal.style.display = "none";
 });
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == newBookModal) {
+    newBookModal.style.display = "none";
+  }
+};
+
 // Set up Library  array and Book constructor
 
 let myLibrary = [];
